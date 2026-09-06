5f41fff1133f0fb746e55a9a6fbb7c2dc2d7bf88
karisaa123o-hub <karisaa123o@gmail.com>
2026-09-06 21:24:23 +0700

    update

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/produk', authJWT, upload.single('file'), (req, res) => {
     });
 });
 
-app.put('/produk/:id_produk', authJWT, (req, res) => {
+app.put('/produk/:id_produk', authJWT, upload.single('file'), (req, res) => {
     const { id_produk } = req.params;
     const { judul, deskripsi, harga, id_kategori } = req.body;
 
@@ -78,16 +78,31 @@ app.put('/produk/:id_produk', authJWT, (req, res) => {
         return res.status(400).json({ message: 'Judul dan harga wajib diisi' });
     }
 
-    const sql = 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=? WHERE id_produk=?';
-    db.query(sql, [judul, deskripsi, harga, id_kategori, id_produk], (err, result) => {
-        if (err) return res.status(500).json({ error: err.sqlMessage });
+    // Ambil nama file lama dari database
+    const cekSql = 'SELECT nama_file FROM produk WHERE id_produk=?';
 
-        if (result.affectedRows === 0) {
-            return res.status(404).json({
-                message: 'Produk tidak ditemukan'
-            });
+    db.query(cekSql, [id_produk], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.sqlMessage });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Produk tidak ditemuakan' });
+        }
+        
+        //Jika ada file baru, guna nama file baru
+        //Jika tidak ada, guna nama file lama.
+        const nama_file = req.file
+        ? req.file.filename
+        : result[0].nama_file;
+
+    const sql = 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=?, nama_file=? WHERE id_produk=?';
+    db.query(sql, [judul, deskripsi, harga, id_kategori, nama_file, id_produk], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.sqlMessage });
         }
         res.json({ message: 'Produk berhasil diupdate!' });
+    });
     });
 });
 
